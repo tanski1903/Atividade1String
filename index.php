ec8ad39e574734afc4a79b22268f314cb4c4c3b6
MatheusBrasil19 <matheusbrasil862@gmail.com>
2022-11-08 19:24:16 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Atividade 1 Função String</title>
+</head>
+<body>
+    <Form action="atividade1.php" method="post">
+        <h1>Contador de Caracteres</h1>
+        <br><br>
+        <label for="p">Digite uma palavra</label>
+        <br><br>
+        <input type="text" id="p" name="p"/>
+        <br><br>
+        <input type="submit" value="Resultado">
+    </Form>
+    
+</body>
+</html>
